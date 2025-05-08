d55333bb911da626d07cb8d978a13530df96d297
Mandesther <138729142+Mandesther@users.noreply.github.com>
2025-05-08 17:08:16 +0200

    first commit

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+
+export default function Home() {
+  return (
+    <main>
+      <h1 className="flex items-center font-bold justify-center text-4xl h-screen">
+      New Project <span className="text-blue-500 pl-1">FOXARD</span>
+      </h1>
+    </main>
+  );
+}
